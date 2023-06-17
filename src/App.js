11c83b8548942f305f499809af04e51efb8c879e
Amrit Pandey <amritpandey8076@gmail.com>
2023-06-17 12:20:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+
+import React,{useState} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [data,setData] = useState([]);
+  const getNews =()=>{
+   axios.get("https://newsapi.org/v2/top-headlines?country=in&apiKey=deedc72c5a484428baad5763cdf70054")
+   .then((response)=>{
+  //  console.log(response);
+   setData(response.data.articles)
+   })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div class="header" >
+  <h1 style={{color:'#fff',paddingLeft:'3rem'}}>News</h1>
+  </div>
+    <div className="container my-3">
+      <button className='btn btn-secondary' onClick={getNews}>Get News</button>
     </div>
+
+    <div className="container">
+      <div className="row">
+        {
+          data.map((value)=>{
+            return(
+              <div className="col">
+              <div className="card" style={{width: '18rem'}}>
+              <img src={value.urlToImage} className="card-img-top" alt="..."/>
+              <div className="card-body">
+                <h5 className="card-title">{value.title}</h5>
+                <p className="card-text">{value.description}</p>
+                <a href="#" className="btn btn-secondary">See More</a>
+              </div>
+            </div>
+            </div>
+            );
+          })
+
+          
+        }
+      </div>
+    </div>
+    </>
   );
 }
 
